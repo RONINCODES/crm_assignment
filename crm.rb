804f852d89d0c720ff33873dc5ff88f5c2dd5d9b
Ronen Annason <ronenannason83@gmail.com>
2017-05-27 18:27:37 -0400

    added method modify_existing_contact to crm class

--- crm.rb
+++ crm.rb
@@ -55,8 +55,23 @@ class CRM
   end
 
   def modify_existing_contact
+    puts "Please enter the First Name associated with the contact you would like to modify? "
+    value_name = gets.chomp
 
+    contact_to_modify = Contact.find_by("first_name", f_name)
+
+    puts "Which attribute would you like to modify? "
+    attribute = gets.chomp
+
+    puts "Please enter your specified modification to selected attribute: "
+    value_modification = gets.chomp
+
+    contact_tomodify.update(attribute, value)
   end
+
+
+
+  
 
   def delete_contact
 
